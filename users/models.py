bd70ffe05e0b11ed28a87ff29109b644532b4571
Gilson <gindrejunior@hotmail.com>
2022-01-28 20:12:38 -0300

    created custom user model and manager

--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Criado o modelo de user manager apenas para futuras alteracoes"""
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('Username is required')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+
+        return self._create_user(username, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """Criado o modelo de usuario apenas para futuras alteracoes"""
+    def __str__(self):
+        return self.email
+
+    objects = UserManager()
