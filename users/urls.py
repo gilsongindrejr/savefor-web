aa4a7dbf46e8fac1404cb6e450f9ea40d45a4f5e
Gilson <gindrejunior@hotmail.com>
2022-01-29 06:53:17 -0300

    added success_url to password change view

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('login', LoginUserView.as_view(), name='login'),
     path('logout', LogoutUserView.as_view(), name='logout'),
-    path('password_change', PasswordChangeUserView.as_view(), name='password_change'),
+    path('password_change', PasswordChangeUserView.as_view(success_url='login'), name='password_change'),
 ]
