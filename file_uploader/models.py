f588e0e3a1bbac4d0805d6f9b4e65b7345dcbd23
Gilson <gindrejunior@hotmail.com>
2022-01-28 23:24:20 -0300

    added author to file model

--- file_uploader/models.py
+++ file_uploader/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class File(models.Model):
-    file = models.FileField(upload_to='/%Y/%m/%d')
+    author = models.ForeignKey(get_user_model(), related_name='files', null=False, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='%Y/%m/%d')
