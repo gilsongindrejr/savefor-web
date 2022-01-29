32031a1162566049a285b8f6919ce1ff6a373a6a
Gilson <gindrejunior@hotmail.com>
2022-01-28 23:25:13 -0300

    created upload view

--- file_uploader/views.py
+++ file_uploader/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import File
+from .forms import FileForm
+
+
+class UploadView(View):
+
+    def post(self, request):
+        context = {}
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_file = File(file=request.FILES['file'])
+            new_file.author = request.user
+            new_file.save()
+            context['message'] = 'Arquivo enviado com sucesso!'
+        form = FileForm()
+        context['form'] = form
+        return render(request, 'file_uploader/upload.html', context)
+
+    def get(self, request):
+        form = FileForm()
+        context = {'form': form}
+        return render(request, 'file_uploader/upload.html', context)
