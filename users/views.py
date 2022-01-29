8c484508ed8a4e9feff0d2c046ff0bf950aa0f37
Gilson <gindrejunior@hotmail.com>
2022-01-28 21:54:50 -0300

    added login required mixin

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LoginUserView(LoginView):
     template_name = 'users/login.html'
 
 
-class LogoutUserView(LogoutView):
+class LogoutUserView(LoginRequiredMixin, LogoutView):
     template_name = 'users/logout.html'
 
 
