e52a6c7d6322acae843b2bce00826cb40ff98935
Gilson <gindrejunior@hotmail.com>
2022-01-30 00:32:02 -0300

    created register user view and url

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import LoginUserView, LogoutUserView, PasswordChangeUserView
+from users.views import LoginUserView, LogoutUserView, PasswordChangeUserView, RegisterUserView
 
 app_name = 'users'
 urlpatterns = [
     path('login', LoginUserView.as_view(), name='login'),
     path('logout', LogoutUserView.as_view(), name='logout'),
     path('password_change', PasswordChangeUserView.as_view(success_url='login'), name='password_change'),
+    path('register', RegisterUserView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
 
 
 class LoginUserView(LoginView):
@@ -14,3 +18,10 @@ class LogoutUserView(LoginRequiredMixin, LogoutView):
 
 class PasswordChangeUserView(PasswordChangeView):
     template_name = 'users/password_change.html'
+
+
+class RegisterUserView(CreateView):
+    template_name = 'users/register.html'
+    model = get_user_model()
+    success_url = reverse_lazy('users:login')
+    form_class = CustomUserCreationForm
