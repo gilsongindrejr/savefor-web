203a250fb808dee1932eb3dcbf0fc281de7a95f8
Gilson <gindrejunior@hotmail.com>
2022-02-03 07:42:34 -0300

    raise exception on not authenticated user

--- file_uploader/views.py
+++ file_uploader/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.exceptions import PermissionDenied
 
 from .models import File
 from .forms import FileForm
@@ -8,6 +9,8 @@ from .forms import FileForm
 class UploadView(View):
 
     def post(self, request):
+        if request.user.is_authenticated is not True:
+            raise PermissionDenied()
         context = {}
         form = FileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -20,6 +23,8 @@ class UploadView(View):
         return render(request, 'file_uploader/upload.html', context)
 
     def get(self, request):
+        if request.user.is_authenticated is not True:
+            raise PermissionDenied()
         form = FileForm()
         context = {'form': form}
         return render(request, 'file_uploader/upload.html', context)
