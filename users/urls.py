9ef6c5bb0a4d0457601d2d2f82bd0d13b9e210bc
Gilson <gindrejunior@hotmail.com>
2022-02-16 22:02:16 -0300

    added authentication form param in login view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import LoginUserView, LogoutUserView, PasswordChangeUserView, RegisterUserView
+from .views import LoginUserView, LogoutUserView, PasswordChangeUserView, RegisterUserView
+from .forms import UserLoginForm
 
 app_name = 'users'
 urlpatterns = [
-    path('login', LoginUserView.as_view(), name='login'),
+    path('login', LoginUserView.as_view(authentication_form=UserLoginForm), name='login'),
     path('logout', LogoutUserView.as_view(), name='logout'),
     path('password_change', PasswordChangeUserView.as_view(success_url='login'), name='password_change'),
     path('register', RegisterUserView.as_view(), name='register'),
